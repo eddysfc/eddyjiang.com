5113e7fc315e62d00b0d8c9b0a95cdd6e6d940e0
eddysfc <eddyjiang55@outlook.com>
2024-12-05 16:14:43 -0600

    added dynamic routing for posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
 import { z, defineCollection } from 'astro:content';
 
 const postCollection = defineCollection({
+    type: "content",
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        date: z.date(),
-        slug: z.string(),
+        date: z.string(),
     }),
 });
 
 export const collections = {
-    "posts": postCollection,
+    posts: postCollection
 };
