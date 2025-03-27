8e7a7930f17e7e20ed140231f686187dbd4af40e
eddysfc <159918813+eddysfc@users.noreply.github.com>
2025-03-26 20:30:45 -0500

    research page (i love conditional rendering)

--- src/content/config.ts
+++ src/content/config.ts
@@ -25,10 +25,20 @@ const researchCollection = defineCollection({
     schema: z.object({
         name: z.string(),
         description: z.optional(z.string()),
-        association: z.optional(z.tuple([z.string(), z.optional(z.string().url())])),
+        association: z.optional(
+            z.union([
+                z.literal("independent"),
+                z.array(
+                    z.union([
+                        z.tuple([z.string(), z.string()]),
+                        z.tuple([z.string(), z.string(), z.string().url()])
+                    ])
+                )
+            ])
+        ),
         startDate: z.optional(z.date()),
         endDate: z.optional(z.date()),
-        link: z.optional(z.tuple([z.string(), z.string().url()])),
+        links: z.optional(z.array(z.tuple([z.string(), z.string().url()]))),
     }),
 });
 
